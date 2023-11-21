1693490028b6b190a8b9226a36b7058ccec3a496
YokotaYusuke <tarut8166@gmail.com>
2023-11-21 14:04:34 +0900

    データベース名を小文字に変更

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
     client: 'pg',
     connection: {
       user: process.env.POSTGRES_USER || 'postgres',
-      database: process.env.POSTGRES_DB || 'DressUpCat',
+      database: process.env.POSTGRES_DB || 'dressupcat',
     },
     migrations: {
       directory: './data/migrations',
